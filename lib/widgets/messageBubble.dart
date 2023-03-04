489bf86d3953250225f2fbc70e27162180e1142f
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:57:57 -0300

    add balão de mensagem

---
+++ lib/widgets/messageBubble.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final Key key;
+  final String userName;
+  final String userImage;
+  final String message;
+  final bool belongsToMe;
+
+  MessageBubble(
+    this.message,
+    this.userName,
+    this.userImage,
+    this.belongsToMe, {
+    this.key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          mainAxisAlignment:
+              belongsToMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration( 
+                color: belongsToMe
+                    ? Colors.grey[300]
+                    : Theme.of(context).focusColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft:
+                      belongsToMe ? Radius.circular(12) : Radius.circular(0),
+                  bottomRight:
+                      belongsToMe ? Radius.circular(0) : Radius.circular(12),
+                ),
+              ),
+              width: 140,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 8,
+              ),
+              child: Column(
+                crossAxisAlignment: belongsToMe
+                    ? CrossAxisAlignment.end
+                    : CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    userName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: belongsToMe
+                          ? Colors.black
+                          : Theme.of(context).appBarTheme,
+                    ),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                      color: belongsToMe
+                          ? Colors.black
+                          : Theme.of(context).appBarTheme,
+                    ),
+                    textAlign: belongsToMe ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: belongsToMe ? null : 128,
+          right: belongsToMe ? 128 : null,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(this.userImage),
+          ),
+        ),
+      ],
+    );
+  }
+}
+  
