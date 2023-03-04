0430cb0ef8ed5d8a73ccbe3e096ecdb6eeb87738
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:51:59 -0300

    add model de chat

---
+++ lib/models/Chat.dart
@@ -0,0 +1,27 @@
+
+// import '../integration/talk/talk_web_service.dart';
+// class ChatData {
+
+
+//   Future<void> createChannel(String channel) async { 
+//     final url = new TalkWebService(false, true);
+
+//     final response = await url.createChannel();
+//     return response;
+//   }
+
+//    Future<Object> loadMessages(String token) async {
+//     final url = new TalkWebService(false, true);
+
+//     final response = await url.loadMessages(token);
+//     return response;
+//   }
+
+//   Future<Object> sendMessage(String message) async {
+//     final url = new TalkWebService(false, true);
+
+//     final response = await url.sendTextMessage(message);
+//     return response;
+//   } 
+
+// }
