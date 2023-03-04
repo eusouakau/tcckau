e8b3c45a5de214b11bcc880b07bf7d6df3e9418f
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:58:17 -0300

    add classe de mensagens

---
+++ lib/widgets/messages.dart
@@ -0,0 +1,15 @@
+//import 'package:pense_responda/widgets/message_bubble.dart';
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        
+      ),
+    );
+  }
+}
