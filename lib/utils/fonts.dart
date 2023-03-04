3708546a76b374555dc1a4f2a173ce8060ac1a9d
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:55:43 -0300

    add const com fontes

---
+++ lib/utils/fonts.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import '../utils/colors.dart';
+
+// class Fonts {
+//   static final TextStyle title = GoogleFonts.notoSans(
+//     color: AppColors.green,
+//     fontSize: 20,
+//     fontWeight: FontWeight.w400,
+//   );
+
