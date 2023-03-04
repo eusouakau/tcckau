9ff152357e94a4fed25af24a382b3b727f3b59d7
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:52:12 -0300

    add mode3l de message

---
+++ lib/models/Message.dart
@@ -0,0 +1,46 @@
+
+class Message {
+  // final User sender;
+  final String time; 
+  final String text;
+  final bool unread;
+
+  Message({
+    // this.sender,
+    this.time = '',
+    this.text = '',
+    this.unread = false,
+  });
+}
+
+List<Message> chats = [
+  Message(
+    // sender: partner,
+    // time: '18:30',
+    // text: 'Olá!',
+    // unread: true,
+  ),
+  Message(
+    // sender: partner2,
+    // time: '16:50',
+    // text: 'Oiii!',
+    // unread: false,
+  ),
+];
+
+
+List<Message> messages = [
+  // Message(
+  //   sender: currentUser,
+  //   time: '18:35',
+  //   text: 'Oieee',
+  //   unread: true,
+  // ),
+  // Message(
+  //   sender: partner,
+  //   time: '18:30',
+  //   text: 'Olá.',
+  //   unread: true,
+  // ),
+];
+
