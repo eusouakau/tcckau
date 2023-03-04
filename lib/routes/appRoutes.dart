5aaa9f867fc58b77e2aa217af715209695bfd5f9
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:53:49 -0300

    add rotas

---
+++ lib/routes/appRoutes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const SPLASH_SCREEN = '/';
+  static const AUTH_SCREEN = '/auth-screen';
+  static const HOME_SCREEN = '/home-screen';
+  static const START_SCREEN = '/start-screen';
+  // static const CHAT_SCREEN = '/chat-screen';
+
+}
