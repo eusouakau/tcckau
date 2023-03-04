27f3d4c38a444763d2dd4075496f89b9b72d7a29
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:55:28 -0300

    add consts com cores

---
+++ lib/utils/colors.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color green = Color(0x50C878);
+  static final Color white = Color(0xFFFFFF);
+  static final Color black = Color(0x000000);
+  static final Color grey = Color(0x4E4E4E);
+  static final Color border = Color(0x707070);
+  static final Color blue = Color(0x3A86FF);
+  static final Color purple = Color(0x697BA8);
+
+ //GameTypeButton
+ static final Color gameTypeButton1 = Color(0x3A86FF);
+ static final Color gameTypeButton2 = Color(0x697BA8);
+ static final Color gameTypeButton3 = Color(0x85CCAF); 
+ static final Color gameTypeButton4 = Color(0xDDDD00);
+ static final Color gameTypeButtonBorder = Color(0x3A86FF);
+
+  //Right
+  static final Color lightGreen = Color(0x66FF00);
+  static final Color darkGreen = Color(0x115500);
+
+  //Error
+  static final Color red = Color(0xFF1100);
+  static final Color darkRed = Color(0xAA0000);
+
+  //Button
+  static final Color greenButton = Color(0xDDDD00);
+  static final Color blueButton = Color(0x3A86FF);
+  static final Color greyButton = Color(0x4E4E4E);
+
+  //Background
+  static final Color darkBackgroundColor = Color(0x3E3E3E);
+  static final Color lightBackgroundColor = Color(0x4E4E4E);
+  
+  //AppBarColor
+  static final Color appBarColor = Color(0x333333);
+
+}
