3da992955e0e3cf558fd29900608715f189540c6
Kauana Henriques <karoltvbl@gmail.com>
2023-03-04 15:52:44 -0300

    add model de search

---
+++ lib/models/Search.dart
@@ -0,0 +1,12 @@
+///import '../integration/search/';
+// import '../integration/users/users_web_service.dart';
+
+// class SearchData {
+
+//   Future<void> searchUser(String name) async {
+//     final url = new UsersWebService(false, false);
+//     final response = await url.searchUser(name); 
+//     return response;
+//   }
+
+// } 
