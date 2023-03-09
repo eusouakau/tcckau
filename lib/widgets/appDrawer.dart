e80710d36e0f41beba6f498f6b984307fe8aedc7
Kauana Henriques <karoltvbl@gmail.com>
2023-03-09 00:48:02 -0300

    ajustes lint

--- lib/widgets/appDrawer.dart
+++ lib/widgets/appDrawer.dart
@@ -1,6 +1,9 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 
 class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
