94b1f5430a87561b559dab9310465dd4e98bb3b6
Kauana Henriques <karoltvbl@gmail.com>
2023-03-09 00:40:15 -0300

    arruma provisóriamnet para nã quebrar outra parte

--- lib/models/Search.dart
+++ lib/models/Search.dart
@@ -1,12 +1,12 @@
-///import '../integration/search/';
+// /import '../integration/search/';
 // import '../integration/users/users_web_service.dart';
 
-// class SearchData {
+class SearchData {
 
-//   Future<void> searchUser(String name) async {
-//     final url = new UsersWebService(false, false);
-//     final response = await url.searchUser(name); 
-//     return response;
-//   }
+  // Future<void> searchUser(String name) async {
+  //   final url = new UsersWebService(false, false);
+  //   final response = await url.searchUser(name); 
+  //   return response;
+  // }
 
-// } 
+} 
