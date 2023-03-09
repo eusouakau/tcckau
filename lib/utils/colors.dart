9737ee46550884b77c2909f34947f6bbe8a69f82
Kauana Henriques <karoltvbl@gmail.com>
2023-03-09 00:45:47 -0300

    substitui final por const

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static final Color green = Color(0x50C878);
-  static final Color white = Color(0xFFFFFF);
-  static final Color black = Color(0x000000);
-  static final Color grey = Color(0x4E4E4E);
-  static final Color border = Color(0x707070);
-  static final Color blue = Color(0x3A86FF);
-  static final Color purple = Color(0x697BA8);
+  static const Color green = Color(0x50C878);
+  static const Color white = Color(0xFFFFFF);
+  static const Color black = Color(0x000000);
+  static const Color grey = Color(0x4E4E4E);
+  static const Color border = Color(0x707070);
+  static const Color blue = Color(0x3A86FF);
+  static const Color purple = Color(0x697BA8);
 
  //GameTypeButton
- static final Color gameTypeButton1 = Color(0x3A86FF);
- static final Color gameTypeButton2 = Color(0x697BA8);
- static final Color gameTypeButton3 = Color(0x85CCAF); 
- static final Color gameTypeButton4 = Color(0xDDDD00);
- static final Color gameTypeButtonBorder = Color(0x3A86FF);
+ static const Color gameTypeButton1 = Color(0x3A86FF);
+ static const Color gameTypeButton2 = Color(0x697BA8);
+ static const Color gameTypeButton3 = Color(0x85CCAF); 
+ static const Color gameTypeButton4 = Color(0xDDDD00);
+ static const Color gameTypeButtonBorder = Color(0x3A86FF);
 
   //Right
-  static final Color lightGreen = Color(0x66FF00);
-  static final Color darkGreen = Color(0x115500);
+  static const Color lightGreen = Color(0x66FF00);
+  static const Color darkGreen = Color(0x115500);
 
   //Error
-  static final Color red = Color(0xFF1100);
-  static final Color darkRed = Color(0xAA0000);
+  static const Color red = Color(0xFF1100);
+  static const Color darkRed = Color(0xAA0000);
 
   //Button
-  static final Color greenButton = Color(0xDDDD00);
-  static final Color blueButton = Color(0x3A86FF);
-  static final Color greyButton = Color(0x4E4E4E);
+  static const Color greenButton = Color(0xDDDD00);
+  static const Color blueButton = Color(0x3A86FF);
+  static const Color greyButton = Color(0x4E4E4E);
 
   //Background
-  static final Color darkBackgroundColor = Color(0x3E3E3E);
-  static final Color lightBackgroundColor = Color(0x4E4E4E);
+  static const Color darkBackgroundColor = Color(0x3E3E3E);
+  static const Color lightBackgroundColor = Color(0x4E4E4E);
   
   //AppBarColor
-  static final Color appBarColor = Color(0x333333);
+  static const Color appBarColor = Color(0x333333);
 
 }
