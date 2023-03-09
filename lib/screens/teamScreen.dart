57a3254ca31a22e27e89dad08333c0b311b52897
Kauana Henriques <karoltvbl@gmail.com>
2023-03-09 00:47:48 -0300

    ajustes lint

--- lib/screens/teamScreen.dart
+++ lib/screens/teamScreen.dart
@@ -3,6 +3,8 @@ import 'package:tcckau/routes/appRoutes.dart';
 import 'package:tcckau/widgets/teamCard.dart';
 
 class TeamScreen extends StatefulWidget {
+  const TeamScreen({super.key});
+
   // const TeamScreen({ Key? key }) : super(key: key);
 
   @override
@@ -14,7 +16,7 @@ class _TeamScreenState extends State<TeamScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
-         title: Text(
+         title: const Text(
           'Iniciar partida',
           style: TextStyle(
             fontSize: 20,
@@ -22,9 +24,9 @@ class _TeamScreenState extends State<TeamScreen> {
           ),
         ),
         centerTitle: true,
-        backgroundColor: Color.fromRGBO(30, 30, 30, 1),
-        leading: new IconButton(
-          icon: new Icon(Icons.arrow_back),
+        backgroundColor: const Color.fromRGBO(30, 30, 30, 1),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.of(context).pushReplacementNamed(AppRoutes.START_SCREEN);
           },
@@ -32,22 +34,22 @@ class _TeamScreenState extends State<TeamScreen> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Color.fromRGBO(40, 40, 40, 1),
           ),
-          padding: EdgeInsets.only(top: 60, bottom: 40),
+          padding: const EdgeInsets.only(top: 60, bottom: 40),
           child: Column(
             children: [
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Column(
                 children: [
                   Container(
-                    margin: EdgeInsets.only(bottom: 20.0),
-                    padding: EdgeInsets.symmetric(
+                    margin: const EdgeInsets.only(bottom: 20.0),
+                    padding: const EdgeInsets.symmetric(
                       vertical: 8,
                       horizontal: 70,
                     ),
-                    child: Text(
+                    child: const Text(
                       'Formar equipe',
                       style: TextStyle(
                         color: Color.fromRGBO(185, 185, 185, 1),
@@ -57,9 +59,9 @@ class _TeamScreenState extends State<TeamScreen> {
                   ),
                 ],
               ),
-              SizedBox(height: 25),
+              const SizedBox(height: 25),
               TeamCard(),
-              SizedBox(height: 25),
+              const SizedBox(height: 25),
             ],
           ),
         ),
